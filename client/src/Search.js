ef9ce56f532ed4c313ef6c3f29a59ad266f32c1a
Anna Langner <annalangner06@gmail.com>
2021-02-08 23:05:46 +0100

    First attempt to add a gallery

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { getPhoto } from './Photo';
+import { Form, Col, Button, Container } from 'react-bootstrap';
+import { Gallery, Item } from 'react-photoswipe-gallery';
+import 'photoswipe/dist/photoswipe.css'
+import 'photoswipe/dist/default-skin/default-skin.css'
 
 
 export class Search extends React.Component {
@@ -44,35 +48,58 @@ export class Search extends React.Component {
 
     let photoList = photos.map((photo) => {
       return (
-        <li key={photo.id}>
-          <p>{photo.user.name}</p>
-          <p>{photo.alt_description}</p>
-          <img src={photo.urls.small} alt='' />
-        </li>
+        <Item 
+          key={photo.id}
+          original={photo.urls.full}
+          thumbnail={photo.urls.thumb}
+          width="1024"
+          height="768"
+          title={`Author: ${photo.user.name}, Description: ${photo.alt_description}`}          
+        >
+          {({ ref, open }) => (
+            <img style={{ cursor: 'pointer', objectFit: 'cover', width: '100%', maxHeight: '100%',}} ref={ref} onClick={open} src={photo.urls.thumb}/>
+          )}                      
+        </Item>
       );        
     });
 
     return(
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-          Photo search engine:
-          <input
-            type='text'
-            onChange={this.handleChange}
-            placeholder='Enter your search term here'
-            value={this.state.searchingText}
-          />
-          </label>
-          <input type='submit' value='Send'/>
-        </form>
+      <Container>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group className='form-group-search'>
+            <Form.Row className="align-items-center">        
+              <Col sm={9}>
+              <Form.Control
+                  type='text'
+                  onChange={this.handleChange}
+                  placeholder='You changed your mind? Enter the new term here'
+                  value={this.state.searchingText}
+                /> 
+              </Col>  
+              <Col sm={3}>
+                  <Button type="submit" className='btn-submit-search'>Submit</Button>
+                </Col>          
+            </Form.Row>
+          </Form.Group>            
+        </Form>
         <div>
-          <ul>
-          { photoList }
-          </ul>
+          <h1>{this.state.searchingText}</h1>
         </div>
-      </div>
+        <div>
+          <Gallery>
+            <div className='gallery-div'
+              style={{
+                display: 'grid',
+                gridTemplateColumns: '240px 171px 171px',
+                gridTemplateRows: '114px 114px',
+                gridGap: 12,
+              }}
+            >
+              { photoList }
+            </div>
+          </Gallery>
+        </div>
+      </Container>
     );
   }
-
 }
